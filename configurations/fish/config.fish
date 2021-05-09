f6a2a549be2ef115af28c6dd04634a07e19e519a
liconaj <liconaj215@gmail.com>
2021-05-09 09:45:45 -0500

    Some changes

--- configurations/fish/config.fish
+++ configurations/fish/config.fish
@@ -3,3 +3,7 @@ set fish_color_search_match --background black
 
 alias rm "gio trash"
 alias emacs 'emacsclient -t --create-frame --alternate-editor=""'
+
+function __fish_command_not_found_handler --on-event fish_command_not_found
+    echo "fish: Unknown command '$argv'"
+end
