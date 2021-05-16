b52bb26e4c1e9fb0844404f75df13cc2f113613c
Josué Licona Mármol <josue@archasus.localdomain>
2021-05-15 20:06:15 -0500

    Some changes

--- configurations/fish/config.fish
+++ configurations/fish/config.fish
@@ -1,8 +1,10 @@
-set fish_greeting "Bienvenido @$USER"
+#set fish_greeting "Bienvenido @$USER"
+set fish_greeting ""
 set fish_color_search_match --background black
 
 alias rm "gio trash"
-alias emacs 'emacsclient -t --create-frame --alternate-editor=""'
+alias edit 'emacsclient -t --alternate-editor="emacs -nw"'
+
 
 function __fish_command_not_found_handler --on-event fish_command_not_found
     echo "fish: Unknown command '$argv'"
