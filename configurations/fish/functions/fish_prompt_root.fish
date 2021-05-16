3805164906b7085720a800e689fb1f3340b61c45
Josué Licona Mármol <josue@archasus.localdomain>
2021-05-15 20:42:55 -0500

    Fish prompt for root

--- configurations/fish/functions/fish_prompt_root.fish
+++ configurations/fish/functions/fish_prompt_root.fish
@@ -1,6 +1,6 @@
 # Defined interactively
 function fish_prompt
 set_color brred --bold
-printf "[%s@%s %s]\# " $USER $hostname (prompt_pwd)
+printf "[%s@%s %s]# " $USER $hostname (prompt_pwd)
 set_color normal
 end
