961a5a3c3d5a39d5778d258de55a2b7f7d5f9ee9
Josué Licona Mármol <josue@archasus.localdomain>
2021-06-13 00:01:05 -0500

    Some changes

--- scripts/globalmenu.sh
+++ scripts/globalmenu.sh
@@ -2,4 +2,3 @@
 
 xfconf-query -c xsettings -p /Gtk/ShellShowsMenubar -n -t bool -s true
 xfconf-query -c xsettings -p /Gtk/ShellShowsAppmenu -n -t bool -s true
-xfconf-query -c xseetings -p /Gtk/Modules -n -t string -s "appmenu-gtk-module"
