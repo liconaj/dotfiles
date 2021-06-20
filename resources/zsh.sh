bca4d1fb889ccfc8a67e9d40aaaea6c5cd819bf6
Josué Licona Mármol <josue@archasus.localdomain>
2021-06-20 00:11:20 -0500

    Some addons and changes

---
+++ resources/zsh.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir -p $ZSH_CUSTOM/themes
+
+wget -xqO $ZSH_CUSTOM/themes/aphrodite.zsh-theme https://raw.github.com/win0err/aphrodite-terminal-theme/master/aphrodite-zsh-theme
+#git clone https://github.com/alejandromume/macos-zsh-theme $ZSH_CUSTOM/themes/macos
